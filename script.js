566774e371a228fea004f6322c5bd12919caf152
unknown <yusuffabdulkadir30@gmail.com>
2023-06-04 22:23:43 +0100

    some modification

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
 const func = (nums) => {
     for (let i = 1; i <= nums; i++) {
         let pattern = '';
         for (let j = 0; j < i; j++) {
-            pattern +='*';
-            console.log(pattern)
-            
+            pattern += '*';
         }
+        console.log(pattern);
     }
 }
-    func(5)
+    func(5);
